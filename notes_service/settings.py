d76b45f416600816528670ad9bee96d69478a7be
Ilya Grynyshyn <grynyshyn080@gmail.com>
2024-04-28 22:42:27 +0300

    Add login url

--- notes_service/settings.py
+++ notes_service/settings.py
@@ -119,7 +119,7 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 from django.urls import reverse, reverse_lazy
 
 LOGIN_URL = reverse_lazy("account:login")
-LOGIN_REDIRECT_URL = reverse_lazy("home")
+LOGIN_REDIRECT_URL = reverse_lazy("notes-list")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
